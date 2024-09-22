fa07ded2135ee9148c2465b668497bd87a315c37
Blackburn <ahmed.ziabat@protonmail.com>
2024-09-22 22:35:22 +0200

    autotools

---
+++ src/conf/generic/build_config.h
@@ -0,0 +1,26 @@
+/* src/conf/generic/build_config.h.  Generated from build_config.h.in by configure.  */
+/* src/conf/generic/build_config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "omen-kernel"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "youremail@example.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "OMEN kernel"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "OMEN kernel 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "omen-kernel"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"
--- src/include/omen/managers/boot/bootloaders/limine/limine.h
+++ src/include/omen/managers/boot/bootloaders/limine/limine.h
@@ -21,39 +21,35 @@
 extern "C" {
 #endif
 
-#include <stdint.h>
+#include <omen/libraries/std/stdint.h>
 
 /* Misc */
 
 #ifdef LIMINE_NO_POINTERS
-#  define LIMINE_PTR(TYPE) uint64_t
+#define LIMINE_PTR(TYPE) uint64_t
 #else
-#  define LIMINE_PTR(TYPE) TYPE
+#define LIMINE_PTR(TYPE) TYPE
 #endif
 
 #ifdef __GNUC__
-#  define LIMINE_DEPRECATED __attribute__((__deprecated__))
-#  define LIMINE_DEPRECATED_IGNORE_START \
-    _Pragma("GCC diagnostic push") \
-    _Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"")
-#  define LIMINE_DEPRECATED_IGNORE_END \
-    _Pragma("GCC diagnostic pop")
+#define LIMINE_DEPRECATED __attribute__((__deprecated__))
+#define LIMINE_DEPRECATED_IGNORE_START                                                                                 \
+    _Pragma("GCC diagnostic push") _Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"")
+#define LIMINE_DEPRECATED_IGNORE_END _Pragma("GCC diagnostic pop")
 #else
-#  define LIMINE_DEPRECATED
-#  define LIMINE_DEPRECATED_IGNORE_START
-#  define LIMINE_DEPRECATED_IGNORE_END
+#define LIMINE_DEPRECATED
+#define LIMINE_DEPRECATED_IGNORE_START
+#define LIMINE_DEPRECATED_IGNORE_END
 #endif
 
-#define LIMINE_REQUESTS_START_MARKER \
-    uint64_t limine_requests_start_marker[4] = { 0xf6b8f4b39de7d1ae, 0xfab91a6940fcb9cf, \
-                                                 0x785c6ed015d3e316, 0x181e920a7852b9d9 };
-#define LIMINE_REQUESTS_END_MARKER \
-    uint64_t limine_requests_end_marker[2] = { 0xadc0e0531bb10d03, 0x9572709f31764c62 };
+#define LIMINE_REQUESTS_START_MARKER                                                                                   \
+    uint64_t limine_requests_start_marker[4] = {0xf6b8f4b39de7d1ae, 0xfab91a6940fcb9cf, 0x785c6ed015d3e316,            \
+                                                0x181e920a7852b9d9};
+#define LIMINE_REQUESTS_END_MARKER uint64_t limine_requests_end_marker[2] = {0xadc0e0531bb10d03, 0x9572709f31764c62};
 
 #define LIMINE_REQUESTS_DELIMITER LIMINE_REQUESTS_END_MARKER
 
-#define LIMINE_BASE_REVISION(N) \
-    uint64_t limine_base_revision[3] = { 0xf9562b2d5c95a6c8, 0x6a7b384944536bdc, (N) };
+#define LIMINE_BASE_REVISION(N) uint64_t limine_base_revision[3] = {0xf9562b2d5c95a6c8, 0x6a7b384944536bdc, (N)};
 
 #define LIMINE_BASE_REVISION_SUPPORTED (limine_base_revision[2] == 0)
 
@@ -89,7 +85,7 @@ struct limine_file {
 
 /* Boot info */
 
-#define LIMINE_BOOTLOADER_INFO_REQUEST { LIMINE_COMMON_MAGIC, 0xf55038d8e2a1202f, 0x279426fcf5f59740 }
+#define LIMINE_BOOTLOADER_INFO_REQUEST {LIMINE_COMMON_MAGIC, 0xf55038d8e2a1202f, 0x279426fcf5f59740}
 
 struct limine_bootloader_info_response {
     uint64_t revision;
@@ -105,7 +101,7 @@ struct limine_bootloader_info_request {
 
 /* Firmware type */
 
-#define LIMINE_FIRMWARE_TYPE_REQUEST { LIMINE_COMMON_MAGIC, 0x8c2f75d90bef28a8, 0x7045a4688eac00c3 }
+#define LIMINE_FIRMWARE_TYPE_REQUEST {LIMINE_COMMON_MAGIC, 0x8c2f75d90bef28a8, 0x7045a4688eac00c3}
 
 #define LIMINE_FIRMWARE_TYPE_X86BIOS 0
 #define LIMINE_FIRMWARE_TYPE_UEFI32 1
@@ -124,7 +120,7 @@ struct limine_firmware_type_request {
 
 /* Stack size */
 
-#define LIMINE_STACK_SIZE_REQUEST { LIMINE_COMMON_MAGIC, 0x224ef0460a8e8926, 0xe1cb0fc25f46ea3d }
+#define LIMINE_STACK_SIZE_REQUEST {LIMINE_COMMON_MAGIC, 0x224ef0460a8e8926, 0xe1cb0fc25f46ea3d}
 
 struct limine_stack_size_response {
     uint64_t revision;
@@ -139,7 +135,7 @@ struct limine_stack_size_request {
 
 /* HHDM */
 
-#define LIMINE_HHDM_REQUEST { LIMINE_COMMON_MAGIC, 0x48dcf1cb8ad2b852, 0x63984e959a98244b }
+#define LIMINE_HHDM_REQUEST {LIMINE_COMMON_MAGIC, 0x48dcf1cb8ad2b852, 0x63984e959a98244b}
 
 struct limine_hhdm_response {
     uint64_t revision;
@@ -154,7 +150,7 @@ struct limine_hhdm_request {
 
 /* Framebuffer */
 
-#define LIMINE_FRAMEBUFFER_REQUEST { LIMINE_COMMON_MAGIC, 0x9d5827dcd881dd75, 0xa3148604f6fab11b }
+#define LIMINE_FRAMEBUFFER_REQUEST {LIMINE_COMMON_MAGIC, 0x9d5827dcd881dd75, 0xa3148604f6fab11b}
 
 #define LIMINE_FRAMEBUFFER_RGB 1
 
@@ -207,7 +203,7 @@ struct limine_framebuffer_request {
 
 /* Terminal */
 
-#define LIMINE_TERMINAL_REQUEST { LIMINE_COMMON_MAGIC, 0xc8ac59310c2b0844, 0xa68d0c7265d38878 }
+#define LIMINE_TERMINAL_REQUEST {LIMINE_COMMON_MAGIC, 0xc8ac59310c2b0844, 0xa68d0c7265d38878}
 
 #define LIMINE_TERMINAL_CB_DEC 10
 #define LIMINE_TERMINAL_CB_BELL 20
@@ -267,28 +263,28 @@ LIMINE_DEPRECATED_IGNORE_END
 
 /* Paging mode */
 
-#define LIMINE_PAGING_MODE_REQUEST { LIMINE_COMMON_MAGIC, 0x95c1a0edab0944cb, 0xa4e5cb3842f7488a }
+#define LIMINE_PAGING_MODE_REQUEST {LIMINE_COMMON_MAGIC, 0x95c1a0edab0944cb, 0xa4e5cb3842f7488a}
 
-#if defined (__x86_64__) || defined (__i386__)
+#if defined(__x86_64__) || defined(__i386__)
 #define LIMINE_PAGING_MODE_X86_64_4LVL 0
 #define LIMINE_PAGING_MODE_X86_64_5LVL 1
 #define LIMINE_PAGING_MODE_MAX LIMINE_PAGING_MODE_X86_64_5LVL
 #define LIMINE_PAGING_MODE_MIN LIMINE_PAGING_MODE_X86_64_4LVL
 #define LIMINE_PAGING_MODE_DEFAULT LIMINE_PAGING_MODE_X86_64_4LVL
-#elif defined (__aarch64__)
+#elif defined(__aarch64__)
 #define LIMINE_PAGING_MODE_AARCH64_4LVL 0
 #define LIMINE_PAGING_MODE_AARCH64_5LVL 1
 #define LIMINE_PAGING_MODE_MAX LIMINE_PAGING_MODE_AARCH64_5LVL
 #define LIMINE_PAGING_MODE_MIN LIMINE_PAGING_MODE_AARCH64_4LVL
 #define LIMINE_PAGING_MODE_DEFAULT LIMINE_PAGING_MODE_AARCH64_4LVL
-#elif defined (__riscv) && (__riscv_xlen == 64)
+#elif defined(__riscv) && (__riscv_xlen == 64)
 #define LIMINE_PAGING_MODE_RISCV_SV39 0
 #define LIMINE_PAGING_MODE_RISCV_SV48 1
 #define LIMINE_PAGING_MODE_RISCV_SV57 2
 #define LIMINE_PAGING_MODE_MAX LIMINE_PAGING_MODE_RISCV_SV57
 #define LIMINE_PAGING_MODE_MIN LIMINE_PAGING_MODE_RISCV_SV39
 #define LIMINE_PAGING_MODE_DEFAULT LIMINE_PAGING_MODE_RISCV_SV48
-#elif defined (__loongarch__) && (__loongarch_grlen == 64)
+#elif defined(__loongarch__) && (__loongarch_grlen == 64)
 #define LIMINE_PAGING_MODE_LOONGARCH64_4LVL 0
 #define LIMINE_PAGING_MODE_MAX LIMINE_PAGING_MODE_LOONGARCH64_4LVL
 #define LIMINE_PAGING_MODE_MIN LIMINE_PAGING_MODE_LOONGARCH64_4LVL
@@ -313,7 +309,7 @@ struct limine_paging_mode_request {
 
 /* 5-level paging */
 
-#define LIMINE_5_LEVEL_PAGING_REQUEST { LIMINE_COMMON_MAGIC, 0x94469551da9b3192, 0xebe5e86db7382888 }
+#define LIMINE_5_LEVEL_PAGING_REQUEST {LIMINE_COMMON_MAGIC, 0x94469551da9b3192, 0xebe5e86db7382888}
 
 LIMINE_DEPRECATED_IGNORE_START
 
@@ -331,13 +327,13 @@ LIMINE_DEPRECATED_IGNORE_END
 
 /* SMP */
 
-#define LIMINE_SMP_REQUEST { LIMINE_COMMON_MAGIC, 0x95a67b819a1b857e, 0xa0b61b723b6a73e0 }
+#define LIMINE_SMP_REQUEST {LIMINE_COMMON_MAGIC, 0x95a67b819a1b857e, 0xa0b61b723b6a73e0}
 
 struct limine_smp_info;
 
 typedef void (*limine_goto_address)(struct limine_smp_info *);
 
-#if defined (__x86_64__) || defined (__i386__)
+#if defined(__x86_64__) || defined(__i386__)
 
 #define LIMINE_SMP_X2APIC (1 << 0)
 
@@ -357,7 +353,7 @@ struct limine_smp_response {
     LIMINE_PTR(struct limine_smp_info **) cpus;
 };
 
-#elif defined (__aarch64__)
+#elif defined(__aarch64__)
 
 struct limine_smp_info {
     uint32_t processor_id;
@@ -376,7 +372,7 @@ struct limine_smp_response {
     LIMINE_PTR(struct limine_smp_info **) cpus;
 };
 
-#elif defined (__riscv) && (__riscv_xlen == 64)
+#elif defined(__riscv) && (__riscv_xlen == 64)
 
 struct limine_smp_info {
     uint64_t processor_id;
@@ -394,7 +390,7 @@ struct limine_smp_response {
     LIMINE_PTR(struct limine_smp_info **) cpus;
 };
 
-#elif defined (__loongarch__) && (__loongarch_grlen == 64)
+#elif defined(__loongarch__) && (__loongarch_grlen == 64)
 
 struct limine_smp_info {
     uint64_t reserved;
@@ -418,16 +414,16 @@ struct limine_smp_request {
 
 /* Memory map */
 
-#define LIMINE_MEMMAP_REQUEST { LIMINE_COMMON_MAGIC, 0x67cf3d9d378a806f, 0xe304acdfc50c3c62 }
+#define LIMINE_MEMMAP_REQUEST {LIMINE_COMMON_MAGIC, 0x67cf3d9d378a806f, 0xe304acdfc50c3c62}
 
-#define LIMINE_MEMMAP_USABLE                 0
-#define LIMINE_MEMMAP_RESERVED               1
-#define LIMINE_MEMMAP_ACPI_RECLAIMABLE       2
-#define LIMINE_MEMMAP_ACPI_NVS               3
-#define LIMINE_MEMMAP_BAD_MEMORY             4
+#define LIMINE_MEMMAP_USABLE 0
+#define LIMINE_MEMMAP_RESERVED 1
+#define LIMINE_MEMMAP_ACPI_RECLAIMABLE 2
+#define LIMINE_MEMMAP_ACPI_NVS 3
+#define LIMINE_MEMMAP_BAD_MEMORY 4
 #define LIMINE_MEMMAP_BOOTLOADER_RECLAIMABLE 5
-#define LIMINE_MEMMAP_KERNEL_AND_MODULES     6
-#define LIMINE_MEMMAP_FRAMEBUFFER            7
+#define LIMINE_MEMMAP_KERNEL_AND_MODULES 6
+#define LIMINE_MEMMAP_FRAMEBUFFER 7
 
 struct limine_memmap_entry {
     uint64_t base;
@@ -449,7 +445,7 @@ struct limine_memmap_request {
 
 /* Entry point */
 
-#define LIMINE_ENTRY_POINT_REQUEST { LIMINE_COMMON_MAGIC, 0x13d86c035a1cd3e1, 0x2b0caa89d8f3026a }
+#define LIMINE_ENTRY_POINT_REQUEST {LIMINE_COMMON_MAGIC, 0x13d86c035a1cd3e1, 0x2b0caa89d8f3026a}
 
 typedef void (*limine_entry_point)(void);
 
@@ -466,7 +462,7 @@ struct limine_entry_point_request {
 
 /* Kernel File */
 
-#define LIMINE_KERNEL_FILE_REQUEST { LIMINE_COMMON_MAGIC, 0xad97e90e83f1ed67, 0x31eb5d1c5ff23b69 }
+#define LIMINE_KERNEL_FILE_REQUEST {LIMINE_COMMON_MAGIC, 0xad97e90e83f1ed67, 0x31eb5d1c5ff23b69}
 
 struct limine_kernel_file_response {
     uint64_t revision;
@@ -481,7 +477,7 @@ struct limine_kernel_file_request {
 
 /* Module */
 
-#define LIMINE_MODULE_REQUEST { LIMINE_COMMON_MAGIC, 0x3e7e279702be32af, 0xca1c4f3bd1280cee }
+#define LIMINE_MODULE_REQUEST {LIMINE_COMMON_MAGIC, 0x3e7e279702be32af, 0xca1c4f3bd1280cee}
 
 #define LIMINE_INTERNAL_MODULE_REQUIRED (1 << 0)
 #define LIMINE_INTERNAL_MODULE_COMPRESSED (1 << 1)
@@ -510,7 +506,7 @@ struct limine_module_request {
 
 /* RSDP */
 
-#define LIMINE_RSDP_REQUEST { LIMINE_COMMON_MAGIC, 0xc5e77b6b397e7b43, 0x27637845accdcf3c }
+#define LIMINE_RSDP_REQUEST {LIMINE_COMMON_MAGIC, 0xc5e77b6b397e7b43, 0x27637845accdcf3c}
 
 struct limine_rsdp_response {
     uint64_t revision;
@@ -525,7 +521,7 @@ struct limine_rsdp_request {
 
 /* SMBIOS */
 
-#define LIMINE_SMBIOS_REQUEST { LIMINE_COMMON_MAGIC, 0x9e9046f11e095391, 0xaa4a520fefbde5ee }
+#define LIMINE_SMBIOS_REQUEST {LIMINE_COMMON_MAGIC, 0x9e9046f11e095391, 0xaa4a520fefbde5ee}
 
 struct limine_smbios_response {
     uint64_t revision;
@@ -541,7 +537,7 @@ struct limine_smbios_request {
 
 /* EFI system table */
 
-#define LIMINE_EFI_SYSTEM_TABLE_REQUEST { LIMINE_COMMON_MAGIC, 0x5ceba5163eaaf6d6, 0x0a6981610cf65fcc }
+#define LIMINE_EFI_SYSTEM_TABLE_REQUEST {LIMINE_COMMON_MAGIC, 0x5ceba5163eaaf6d6, 0x0a6981610cf65fcc}
 
 struct limine_efi_system_table_response {
     uint64_t revision;
@@ -556,7 +552,7 @@ struct limine_efi_system_table_request {
 
 /* EFI memory map */
 
-#define LIMINE_EFI_MEMMAP_REQUEST { LIMINE_COMMON_MAGIC, 0x7df62a431d6872d5, 0xa4fcdfb3e57306c8 }
+#define LIMINE_EFI_MEMMAP_REQUEST {LIMINE_COMMON_MAGIC, 0x7df62a431d6872d5, 0xa4fcdfb3e57306c8}
 
 struct limine_efi_memmap_response {
     uint64_t revision;
@@ -574,7 +570,7 @@ struct limine_efi_memmap_request {
 
 /* Boot time */
 
-#define LIMINE_BOOT_TIME_REQUEST { LIMINE_COMMON_MAGIC, 0x502746e184c088aa, 0xfbc5ec83e6327893 }
+#define LIMINE_BOOT_TIME_REQUEST {LIMINE_COMMON_MAGIC, 0x502746e184c088aa, 0xfbc5ec83e6327893}
 
 struct limine_boot_time_response {
     uint64_t revision;
@@ -589,7 +585,7 @@ struct limine_boot_time_request {
 
 /* Kernel address */
 
-#define LIMINE_KERNEL_ADDRESS_REQUEST { LIMINE_COMMON_MAGIC, 0x71ba76863cc55f63, 0xb2644a48c516a487 }
+#define LIMINE_KERNEL_ADDRESS_REQUEST {LIMINE_COMMON_MAGIC, 0x71ba76863cc55f63, 0xb2644a48c516a487}
 
 struct limine_kernel_address_response {
     uint64_t revision;
@@ -605,7 +601,7 @@ struct limine_kernel_address_request {
 
 /* Device Tree Blob */
 
-#define LIMINE_DTB_REQUEST { LIMINE_COMMON_MAGIC, 0xb40ddb48fb54bac7, 0x545081493f81ffb7 }
+#define LIMINE_DTB_REQUEST {LIMINE_COMMON_MAGIC, 0xb40ddb48fb54bac7, 0x545081493f81ffb7}
 
 struct limine_dtb_response {
     uint64_t revision;
